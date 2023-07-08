51e0f23158d9ded699af6e4b15ba4c57bbcbaa46
Juan Pregliasco <juanpipre@outlook.com>
2023-07-08 19:27:34 -0300

    Finalizar entidades

---
+++ src/main/java/com/api/tareas/entidades/Tarea.java
@@ -0,0 +1,57 @@
+
+package com.api.tareas.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "tareas")
+public class Tarea {
+
+    public Tarea() {
+    }
+
+    public Tarea(Long tareaId, String titulo, String descripcion, Date fechaCaducidad) {
+        this.tareaId = tareaId;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    @Id
+    @Column(name = "tarea_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tareaId;
+    
+    @Column(unique = true, nullable = false)
+    private String titulo;
+   
+    @Lob
+    @Column(unique = true, nullable = false)
+    private String descripcion;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fecha_caducidad")
+    private Date fechaCaducidad;
+    
+    @Column(nullable = false)
+    private boolean completada = false;
+    
+    @ManyToOne()
+    @JoinColumn(name="usuario_id")
+    private Usuario usuario;
+
+}
---
+++ src/main/java/com/api/tareas/entidades/Usuario.java
@@ -0,0 +1,55 @@
+
+package com.api.tareas.entidades;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "usuarios")
+public class Usuario {
+
+    public Usuario() {
+    }
+
+    public Usuario(Long usuarioId, String nombre, String apellido, String username, String password) {
+        this.usuarioId = usuarioId;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.username = username;
+        this.password = password;
+    }
+    
+    @Id
+    @Column(name = "usuario_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long usuarioId;
+    
+    @Column(nullable  = false)
+    private String nombre;
+    
+   @Column(nullable  =false)
+    private String apellido;
+   
+   @Column(unique = true, nullable  =false)
+    private String username;
+   
+   @Column(nullable  =false)
+    private String password;
+   
+   @OneToMany(mappedBy = "usuario",cascade = CascadeType.ALL)
+   private Set<Tarea> tareas = new HashSet<>();
+   
+   
+   
+}
