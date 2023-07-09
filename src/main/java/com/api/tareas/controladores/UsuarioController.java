d965d1b9b1560a24e7c6b3cfec11e17981861ed2
Juan Pregliasco <juanpipre@outlook.com>
2023-07-09 15:34:48 -0300

    Controlador usuario

--- src/main/java/com/api/tareas/controladores/UsuarioController.java
+++ src/main/java/com/api/tareas/controladores/UsuarioController.java
@@ -1,8 +1,11 @@
 
 package com.api.tareas.controladores;
 
+import com.api.tareas.entidades.Usuario;
 import com.api.tareas.servicios.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,11 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
     
+    @PostMapping
+    public ResponseEntity<Usuario> crearUsuario(Usuario usuario){
+         usuarioService.guardarUsuario(usuario);
+         return ResponseEntity.ok(usuario);
+    }
     
     
 }
