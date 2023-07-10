c2a1845e29764c600a20a6576b9ee1e7b1150d36
Juan Pregliasco <juanpipre@outlook.com>
2023-07-10 20:45:43 -0300

    Añadir dependencias y entidades de request y response

---
+++ src/main/java/com/api/tareas/entidades/JwtResponse.java
@@ -0,0 +1,25 @@
+
+package com.api.tareas.entidades;
+
+
+public class JwtResponse {
+    
+    private String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public JwtResponse() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+     
+    
+}
