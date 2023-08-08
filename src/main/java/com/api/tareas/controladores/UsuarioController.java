5d7e8d83cae7950a53dc879d3129f3077432acc1
Juan Pregliasco <juanpipre@outlook.com>
2023-08-08 12:54:16 -0300

    Solucionar error de CORS

--- src/main/java/com/api/tareas/controladores/UsuarioController.java
+++ src/main/java/com/api/tareas/controladores/UsuarioController.java
@@ -5,6 +5,7 @@ import com.api.tareas.servicios.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class UsuarioController {
 
     @Autowired
